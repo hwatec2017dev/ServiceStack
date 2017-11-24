26f73d71440eccd4006a4401f16febdf83cbf023
申行 <binary2015nov@outlook.com>
2017-11-24 11:39:30 +0800

    fix build

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
 REM %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
 REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0
@@ -8,4 +8,4 @@ REM %MSBUILD% build-deps.proj /target:Build /property:Configuration=Release;RELE
 REM Debug Task
 REM C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /t:rebuild /debug C:\src\ServiceStack\tests\RazorRockstars.BuildTask\RazorRockstars.BuildTask.csproj
 
-msbuild /p:Configuration=Release ..\src\ServiceStack.sln
+%msbuild% /p:Configuration=Release ..\src\ServiceStack.sln
